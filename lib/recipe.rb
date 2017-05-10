e4db95d36c052d6ce069c3f4d0a14fa1ce837d36
Grady Shelton and Jennifer Kinsey <grady@>
2017-05-10 16:27:52 -0700

    adds validation and callback methods on the Recipe class

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,6 +1,29 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
   has_and_belongs_to_many :tags
+  validates(:title, {:presence => true, :length => { :maximum => 50, :minimum => 3 }})
+  validates(:instructions, presence: true)
+  validates(:rating, presence: true)
+  before_save(:titlecase, :check_rating)
+
+
+private
+  define_method(:titlecase) do
+    words = self.title.split(" ")
+    words.each do |word|
+      word.capitalize!
+    end
+    self.title = words.join(" ")
+  end
+
+  def check_rating
+    if(self.rating > 5)
+      self.rating = 5
+    elsif self.rating < 1
+      self.rating = 1
+    end
+  end
+
 end
 
 class Ingredient < ActiveRecord::Base
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -15,7 +15,36 @@ describe(Recipe) do
     test_recipe.tags.push(test_tag1, test_tag2, test_tag3)
     expect(test_recipe.tags).to eq([test_tag1, test_tag2, test_tag3])
   end
+
+  it("returns false if it's too long or too short of a recipe title") do
+    test_recipe = Recipe.create({title: "Pi", instructions: "place frozen pot pie in oven", rating: 3})
+    expect(test_recipe.save).to eq(false)
+    test_recipe2 = Recipe.create({title: "Piheoiufweudhzckuvhuzchivcivycxiyicuyviuxycivuycxiuyicxuyviuyxiuvycivuyxicyv", instructions: "place frozen pot pie in oven", rating: 3})
+    expect(test_recipe2.save).to eq(false)
+  end
+
+  describe("#titlecase") do
+    it("titlecases the title of the recipe") do
+      test_recipe = Recipe.create({title: "spaghetti sandwich", instructions: "place frozen pot pie in oven", rating: 3})
+      expect(test_recipe.title).to eq("Spaghetti Sandwich")
+    end
+  end
+
+  it("ensures there are instructions") do
+    test_recipe = Recipe.create({title: "spaghetti sandwich", instructions: "", rating: 3})
+    expect(test_recipe.save).to eq(false)
+  end
+
+  describe("#check_rating") do
+    it('ensures the rating value is between 1 and 5') do
+      test_recipe = Recipe.create({title: "spaghetti sandwich", instructions: "place frozen pot pie in oven", rating: 20})
+      expect(test_recipe.rating).to eq(5)
+      test_recipe2 = Recipe.create({title: "spaghetti sandwich", instructions: "place frozen pot pie in oven", rating: -20})
+      expect(test_recipe2.rating).to eq(1)
+    end
+  end
 end
+
 
 describe(Ingredient) do
   it("has a recipe") do
